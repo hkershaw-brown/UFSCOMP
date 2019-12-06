ea3203c10b7d1d30e27a4eaf1c5283df31f4b7fd
Helen <helen_kershaw@brown.edu>
2019-12-06 15:27:49 -0500

    git repo check script

---
+++ all_status.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo UFSCOMP $pwd
+
+rootdir=$(pwd)
+
+components=( "."                 \
+             "cime"              \
+             "components/cam"    \
+             "components/cice"   \
+             "components/cism"   \
+             "components/clm"    \
+             "components/mom"    \
+             "components/mosart" \
+             "components/pop"   \
+             "components/ww3") 
+
+for comp in "${components[@]}"
+do
+  cd $comp
+  echo "====="
+  echo " "$comp
+  echo "====="
+  git status | grep modified
+  cd $rootdir
+done
