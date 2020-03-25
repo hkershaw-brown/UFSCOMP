8b92b5c45c4046433d518f0bd21d564c8b0710c2
Helen <helen_kershaw@brown.edu>
2020-03-25 13:42:45 -0400

    changes to all_status from pre_orbit

--- all_status.sh
+++ all_status.sh
@@ -21,7 +21,22 @@ do
   echo "====="
   echo " "$comp
   echo "====="
-  git status | grep modified
-  #git remote -v
+  #git status | grep modified
+  mods=$(git status | grep modified)
+  echo -e "\e[31m$mods\e[0m"
+  branchname=$(git rev-parse --abbrev-ref HEAD)
+  remotename=$(git remote -v | grep fetch)
+  echo $branchname '::' $remotename
   cd $rootdir
 done
+
+cd cime/src/drivers/nuopc
+  echo "====="
+  echo " " cime/src/drivers/nuopc
+  echo "====="
+
+branchname=$(git rev-parse --abbrev-ref HEAD)
+remotename=$(git remote -v | grep fetch)
+mods=$(git status | grep modified)
+echo -e "\e[31m$mods\e[0m"
+echo $branchname '::' $remotename
